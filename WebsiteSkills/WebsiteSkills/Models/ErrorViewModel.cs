b56dedfcc82401098664daea59ec30b43a0a8b20
Francisco Pires <franciscop11@outlook.pt>
2024-05-07 11:46:16 +0100

    Commit inicial.

---
+++ WebsiteSkills/WebsiteSkills/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace WebsiteSkills.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
