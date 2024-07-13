6885c643afe16391c786ada39cbfaa8a7621bd05
Francisco Pires <franciscop11@outlook.pt>
2024-07-13 22:07:06 +0100

    Adição Fetch Skills

--- react-websiteskills/src/Pages/Skills.js
+++ react-websiteskills/src/Pages/Skills.js
@@ -1,38 +1,66 @@
 import React from "react";
 import "../Styles/Skills.css";
 import { Link } from "react-router-dom";
-import Button from 'react-bootstrap/Button';
+import Button from "react-bootstrap/Button";
 import "../Styles/Skills.css";
-import { useState, useEffect } from 'react';
+import { useState, useEffect } from "react";
 
 function Skills() {
-    return <div className="Skills">
-      
-            <div className="OpcoesSkills">
-              <h1> Skills</h1>
-              <Link to="/SkillPages/EditarSkills">
-                <Button variant="secondary">Editar</Button>
-              </Link>
-              <Link to="/SkillPages/EliminarSkills">
-                <Button variant="secondary">Apagar</Button>
-              </Link>
-              <Link to="/SkillPages/AnuncioSkills">
-                <Button variant="secondary">Anuncio</Button>
-              </Link>
-              <Link to="/SkillPages/CriarAnuncioSkills">
-                <Button variant="secondary">Criar Anuncio</Button>
-              </Link>
-              <Link to="/SkillPages/Recursoskills">
-                <Button variant="secondary">Recursos</Button>
-              </Link>
-            </div>
+  const [skills, setSkills] = useState([]);
 
-            <div className="Criação Skill">
-            <Link to="/SkillPages/CriarSkills">
-                <Button variant="dark">Criar Nova Skill</Button>
-              </Link>
+  useEffect(() => {
+    fetchSkills();
+  }, []);
+
+  const fetchSkills = () => {
+    fetch("https://localhost:7263/api/ApiSkills/GetAllSkills", {
+      headers: {
+        accept: "application/json",
+      },
+    })
+      .then((response) => response.json())
+      .then((data) => setSkills(data))
+      .catch((error) => console.log(error));
+  };
+
+  return (
+    <div className="Skills">
+      <div className="OpcoesSkills">
+        <h1> Skills</h1>
+        <Link to="/SkillPages/EditarSkills">
+          <Button variant="secondary">Editar</Button>
+        </Link>
+        <Link to="/SkillPages/EliminarSkills">
+          <Button variant="secondary">Apagar</Button>
+        </Link>
+        <Link to="/SkillPages/AnuncioSkills">
+          <Button variant="secondary">Anuncio</Button>
+        </Link>
+        <Link to="/SkillPages/CriarAnuncioSkills">
+          <Button variant="secondary">Criar Anuncio</Button>
+        </Link>
+        <Link to="/SkillPages/Recursoskills">
+          <Button variant="secondary">Recursos</Button>
+        </Link>
+      </div>
+
+      <div className="Criação Skill">
+        <Link to="/SkillPages/CriarSkills">
+          <Button variant="dark">Criar Nova Skill</Button>
+        </Link>
+      </div>
+
+      <div>
+          {skills.map(skill => (
+            <div key={skill.skillsId} className="skill-item">
+            <img src={`https://localhost:7263/Imagens/${skill.imagem}`} alt={skill.nome} />
+              <h3>{skill.nome}</h3>
+              <p>{skill.descricao}</p>
             </div>
-    
-          </div>; 
-  }
-  export default Skills;
+          ))}
+        </div>
+
+    </div>
+  );
+}
+export default Skills;
