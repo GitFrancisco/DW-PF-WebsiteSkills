eb33a2e2345ba9f4fee0b95f239c75a4bc2fbab1
Daniel Marques <daniel23marq@gmail.com>
2024-07-13 22:37:00 +0100

    Correções Skills.js

--- react-websiteskills/src/Pages/Skills.js
+++ react-websiteskills/src/Pages/Skills.js
@@ -33,23 +33,34 @@ function Skills() {
         <Link to="/SkillPages/EliminarSkills">
           <Button variant="secondary">Apagar</Button>
         </Link>
-        <Link to="/SkillPages/AnuncioSkills">
+        <Link to="/SkillPages/AnunciosSkills">
           <Button variant="secondary">Anuncio</Button>
         </Link>
-        <Link to="/SkillPages/CriarAnuncioSkills">
+        <Link to="/SkillPages/CriarAnunciosSkills">
           <Button variant="secondary">Criar Anuncio</Button>
         </Link>
-        <Link to="/SkillPages/Recursoskills">
+        <Link to="/SkillPages/RecursosSkills">
           <Button variant="secondary">Recursos</Button>
         </Link>
       </div>
 
-            <div className="Criação Skill">
-            <Link to="/SkillPages/CriarSkills">
-                <Button variant="dark">Criar Nova Skill</Button>
-              </Link>
+      <div className="Criação Skill">
+        <Link to="/SkillPages/CriarSkills">
+          <Button variant="dark">Criar Nova Skill</Button>
+        </Link>
+      </div>
+
+      <div>
+          {skills.map(skill => (
+            <div key={skill.skillsId} className="skill-item">
+            <img src={`https://localhost:7263/Imagens/${skill.imagem}`} alt={skill.nome} />
+              <h3>{skill.nome}</h3>
+              <p>{skill.descricao}</p>
             </div>
-    
-          </div>; 
-  }
-  export default Skills;
+          ))}
+        </div>
+
+    </div>
+  );
+}
+export default Skills;
